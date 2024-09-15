f187d9b49fdb935de1c80491eaa0b20b18a93d0c
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2024-09-15 20:32:38 +0100

    Character moving in units

--- UnstableRealityUnity/Assets/Scripts/EntityMover.cs
+++ UnstableRealityUnity/Assets/Scripts/EntityMover.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public static class ExtMets
 {
-    public static Vector3 V2to3(this Vector2 vec)
+    public static Vector3 V2to3(this Vector2 vec, float ZValue)
     {
-        return new Vector3(vec.x, 0f, vec.y);
+        return new Vector3(vec.x, vec.y, ZValue);
     }
 
     public static Vector2 V3to2(this Vector3 vec)
     {
-        return new Vector2(vec.x, vec.z);
+        return new Vector2(vec.x, vec.y);
     }
 
     public static Vector2Int V2toI(this Vector2 vec)
     {
-        return new Vector2Int((int)vec.x, (int)vec.y);
+        return new Vector2Int(Mathf.RoundToInt(vec.x), Mathf.RoundToInt(vec.y));
     }
 
     public static Vector2 V2ItoF(this Vector2Int vec)
@@ -25,9 +25,9 @@ public static class ExtMets
         return new Vector2(vec.x, vec.y);
     }
 
-    public static Vector3 V2Ito3(this Vector2Int vec)
+    public static Vector3 V2Ito3(this Vector2Int vec, float ZValue)
     {
-        return vec.V2ItoF().V2to3();
+        return vec.V2ItoF().V2to3(ZValue);
     }
 
     public static Vector2Int V3toI(this Vector3 vec)
@@ -40,20 +40,26 @@ public class EntityMover : MonoBehaviour
 {
 
     public Vector2Int? Destination { get; private set; }
-    [Range(0,5)]
+    [Range(0, 5)]
     public float MoveSpeed = 1f;
     protected float TimeSinceDestSet = -1f;
 
+    protected float ZValue;
+
     public void SetDesiredDirection(Vector2 dir)
     {
-        Destination = (transform.position + dir.V2to3()).V3toI();
+        if (Destination != null)
+        {
+            return;
+        }
+        Destination = (transform.position + dir.V2to3(ZValue)).V3toI();
         TimeSinceDestSet = 0f;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ZValue = transform.position.z;
     }
 
     // Update is called once per frame
@@ -66,17 +72,29 @@ public class EntityMover : MonoBehaviour
     {
         TimeSinceDestSet += Time.deltaTime;
 
-        if(Destination == null)
+        if (Destination == null)
         {
             return;
         }
-        Vector3 moveDir = Destination.Value.V2Ito3() - transform.position;
-
-        transform.position += moveDir.normalized * MoveSpeed * Time.deltaTime;
-
-        if (transform.position == Destination.Value.V2Ito3())
+        Vector3 vecToDest = Destination.Value.V2Ito3(ZValue) - transform.position;
+        Vector3 moveVec = vecToDest.normalized * MoveSpeed * Time.deltaTime;
+        if (moveVec.sqrMagnitude > vecToDest.sqrMagnitude)
         {
+            moveVec = vecToDest;
+        }
+        transform.position += moveVec;
+
+        if (CloseEnough())
+        {
+            transform.position = Destination.Value.V2Ito3(ZValue);
             Destination = null;
         }
+    }
+
+    protected bool CloseEnough()
+    {
+        if (Destination == null) { return false; }
+
+        return (Destination.Value.V2Ito3(ZValue) - transform.position).sqrMagnitude < 0.001f;
     }
 }
--- UnstableRealityUnity/Assets/Scripts/PlayerController.cs
+++ UnstableRealityUnity/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(EntityMover))]
 public class PlayerController : MonoBehaviour
 {
+    private EntityMover Mover;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mover = GetComponent<EntityMover>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        InputUpdate();
+    }
+
+    void InputUpdate()
+    {
+        Vector2 moveDir = Input.GetAxisRaw("Horizontal") * Vector2.right + Input.GetAxisRaw("Vertical") * Vector2.up;
         
+        if (Mathf.Abs(moveDir.y) >= Mathf.Abs(moveDir.x))
+        {
+            moveDir.x = 0;
+        }
+        else
+        {
+            moveDir.y = 0;
+        }
+
+        if(moveDir != Vector2.zero)
+        {
+            Mover.SetDesiredDirection(moveDir);
+        }
+
     }
 }
