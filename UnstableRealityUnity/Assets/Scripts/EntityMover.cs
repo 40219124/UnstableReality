7e972a368b69109c290e604fe0a36a95c0119324
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2024-09-19 20:06:21 +0100

    Player collides with walls

--- UnstableRealityUnity/Assets/Scripts/EntityMover.cs
+++ UnstableRealityUnity/Assets/Scripts/EntityMover.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public static class ExtMets
 {
@@ -15,7 +16,7 @@ public static class ExtMets
         return new Vector2(vec.x, vec.y);
     }
 
-    public static Vector2Int V2toI(this Vector2 vec)
+    public static Vector2Int V2toI2(this Vector2 vec)
     {
         return new Vector2Int(Mathf.RoundToInt(vec.x), Mathf.RoundToInt(vec.y));
     }
@@ -30,9 +31,14 @@ public static class ExtMets
         return vec.V2ItoF().V2to3(ZValue);
     }
 
-    public static Vector2Int V3toI(this Vector3 vec)
+    public static Vector2Int V3toI2(this Vector3 vec)
     {
-        return vec.V3to2().V2toI();
+        return vec.V3to2().V2toI2();
+    }
+
+    public static Vector3Int V3toI3(this Vector3 vec)
+    {
+        return new Vector3Int(Mathf.RoundToInt(vec.x), Mathf.RoundToInt(vec.y), Mathf.RoundToInt(vec.z));
     }
 }
 
@@ -41,8 +47,15 @@ public enum eDirections
     none = -1, up, right, down, left
 }
 
+
 public class EntityMover : MonoBehaviour
 {
+    public enum eSetMoveResult
+    {
+        tooSoon = -1,
+        success = 0,
+        blocked = 1
+    }
 
     public Vector2Int? Destination { get; private set; }
     public eDirections TravelDir { get; private set; }
@@ -56,39 +69,89 @@ public class EntityMover : MonoBehaviour
     protected Vector2? AttemptedDesDir = null;
     protected float TimeAttemptedSet = -1f;
 
-    public void SetDesiredDirection(Vector2 dir)
+    [SerializeField]
+    protected LayerMask MoveBlocker;
+
+    [SerializeField]
+    protected Tilemap Tilemap;
+
+    public eSetMoveResult SetDesiredDirection(Vector2 dir)
     {
+        eSetMoveResult outcome = eSetMoveResult.tooSoon;
+
         if (Destination != null)
         {
             AttemptedDesDir = dir;
             TimeAttemptedSet = Time.time;
-            return;
+            return outcome;
         }
-        Destination = (transform.position + dir.V2to3(ZValue)).V3toI();
-        TimeSinceDestSet = 0f;
+        Vector3 desPos = (transform.position + dir.V2to3(0f));
+
+        Vector3Int tilePos = Tilemap.WorldToCell(desPos);
+        TileBase tile = Tilemap.GetTile(tilePos);
+
+
+        if (tile != null)
+        {
+            outcome = eSetMoveResult.blocked;
+        }
+
+        /* RaycastHit2D hit = Physics2D.Raycast(desPos, Vector2.zero);//, 100f, MoveBlocker.value);
+        if (hit.collider != null && !hit.collider.CompareTag("MoveBlock"))
+        {
+            outcome = eSetMoveResult.blocked;
+        }*/
+
+        if (outcome != eSetMoveResult.blocked)
+        {
+            Destination = desPos.V3toI2();
+            TimeSinceDestSet = 0f;
+        }
 
         if (dir.x != 0f)
         {
             if (dir.x > 0f)
             {
-                TravelDir = FacingDir = eDirections.right;
+                if (outcome != eSetMoveResult.blocked)
+                {
+                    TravelDir = eDirections.right;
+                }
+                FacingDir = eDirections.right;
             }
             else
             {
-                TravelDir = FacingDir = eDirections.left;
+                if (outcome != eSetMoveResult.blocked)
+                {
+                    TravelDir = eDirections.left;
+                }
+                FacingDir = eDirections.left;
             }
         }
         else
         {
             if (dir.y > 0f)
             {
-                TravelDir = FacingDir = eDirections.up;
+                if (outcome != eSetMoveResult.blocked)
+                {
+                    TravelDir = eDirections.up;
+                }
+                FacingDir = eDirections.up;
             }
             else
             {
-                TravelDir = FacingDir = eDirections.down;
+                if (outcome != eSetMoveResult.blocked)
+                {
+                    TravelDir = eDirections.down;
+                }
+                FacingDir = eDirections.down;
             }
         }
+
+        if (outcome != eSetMoveResult.blocked)
+        {
+            outcome = eSetMoveResult.success;
+        }
+        return outcome;
     }
 
     // Start is called before the first frame update
@@ -112,7 +175,8 @@ public class EntityMover : MonoBehaviour
         {
             return;
         }
-        Vector3 vecToDest = Destination.Value.V2Ito3(ZValue) - transform.position;
+        Vector3 vecToDest = Destination.Value.V2Ito3(0f) - transform.position;
+        vecToDest.z = 0;
         Vector3 moveVec = vecToDest.normalized * MoveSpeed * Time.deltaTime;
         if (moveVec.sqrMagnitude > vecToDest.sqrMagnitude)
         {
@@ -140,6 +204,6 @@ public class EntityMover : MonoBehaviour
     {
         if (Destination == null) { return false; }
 
-        return (Destination.Value.V2Ito3(ZValue) - transform.position).sqrMagnitude < 0.001f;
+        return (Destination.Value.V2Ito3(transform.position.z) - transform.position).sqrMagnitude < 0.001f;
     }
 }
